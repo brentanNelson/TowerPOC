e8f3bf9149593865125384fd59853c4a3654d035
brentanNelson <brentan.nelson@hotmail.com>
2017-04-15 11:05:14 +1000

    basic camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public int speed = 50;
+    public int rotateSpeed = 10;
+    public bool inFirstPerson = false;
+    public float smooth = 2.0F;
+    public float tiltAngle = 30.0F;
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!inFirstPerson)
+        {
+            //TODO: y axis feels weird
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                var rotation = Quaternion.Euler(0, rotateSpeed, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * smooth);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                var rotation = Quaternion.Euler(0, -rotateSpeed, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * smooth);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+
+	// Use this for initialization
+    public bool inFirstPersonView = false;
+    private Rigidbody rigidBody;
+    private CameraController mainCam;
+    private Vector3 camCurrentPosition;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        var findCamera = GameObject.Find("Main Camera");
+        mainCam = findCamera.GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            ToggleCamera();
+        }
+    }
+
+    public void ToggleCamera()
+    {
+        inFirstPersonView = !inFirstPersonView ? true : false;
+        if (inFirstPersonView)
+        {
+            //zoom in
+            mainCam.inFirstPerson = true;
+            camCurrentPosition = Camera.main.transform.position;
+            Camera.main.transform.position = rigidBody.transform.position;
+        }
+        else
+        {
+            //zoom out
+            Camera.main.transform.position = camCurrentPosition;
+        }
+    }
+}
